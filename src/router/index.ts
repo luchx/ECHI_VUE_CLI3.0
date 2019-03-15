6211feba566db43497f9c7eb3e759c6a6f258039
Echi <914743765@qq.com>
2019-03-15 17:36:17 +0800

    修改路由信息

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,16 +2,12 @@ import Vue from 'vue';
 import Router, {
   RouteConfig,
 } from 'vue-router';
-import Home from '@/views/Home.vue';
+import filterRouter from '@/router/filterRouter';
 
 Vue.use(Router);
 
 const routes: RouteConfig[] = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
+  ...filterRouter,
   {
     path: '/about',
     name: 'about',
